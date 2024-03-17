0b63bb7201c9c9e4284f84a51a05f2fee0dce52d
ReginYuan <49477488+ReginYuan@users.noreply.github.com>
2024-03-17 23:24:48 +0800

    feat: 修改代码

--- vite-vue3/src/App.jsx
+++ vite-vue3/src/App.jsx
@@ -1,9 +1,17 @@
 import { defineComponent } from "vue";
-import classes from "@styles/test-module.css";
+import classes from "@styles/test.module.css";
+import "@styles/test.less";
+import { a } from "./test";
+import logoImg from "./assets/vue.svg";
 export default defineComponent({
   setup() {
     return () => {
-      return <div class={`root ${classes.moduleClass}`}> Hellow Vue3 Jsx</div>;
+      return (
+        <div>
+          <div class={`root ${classes.moduleClass}`}> Hellow {a.name}</div>
+          <img src={logoImg} alt="" />
+        </div>
+      );
     };
   }
 });
---
+++ vite-vue3/src/test.ts
@@ -0,0 +1,8 @@
+import { A } from "./types";
+import test from "@styles/test.less?url";
+console.log(test)
+import test1 from "@styles/test.less?raw";
+console.log(test1)
+export const a: A = {
+  name: 'Jack'
+}
